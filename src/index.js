6f32132856cd072b79413a6238c08ad98135714a
speakingcode <dlissner@gmail.com>
2018-06-15 23:11:16 -0500

    Add react-redux and react-router etc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React                  from 'react';
+import ReactDOM               from 'react-dom';
+import {
+  Route,
+  BrowserRouter
+}                             from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore }        from 'redux'
+import { Provider }           from 'react-redux'
+
+import './index.css';
+import App                    from './App';
+
+import registerServiceWorker  from './registerServiceWorker';
+
+const store = createStore(()=>({}))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
